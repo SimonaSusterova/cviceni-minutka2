d5868dd663f3ac906e2ff53efe3d1eea6c9d8aaa
Simona Šusterová <simsusterova@gmail.com>
2025-05-12 19:09:29 +0200

    hotovo

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-'use strict';
 
-console.log('funguju!');
+//console.log('funguju!');
+
+const nastartujMinutku = (e) => {
+  e.preventDefault();
+  let time = Number(document.querySelector('.time-input').value);
+
+  const sekundyElm = document.querySelector('.alarm__seconds');
+  const alarmElm = document.querySelector('.alarm');
+
+  // pro opakované použití
+  alarmElm.classList.remove('alarm--ring');
+
+  sekundyElm.textContent = String(time);
+
+  const odpocet = () => {
+    if (time > 0) {
+      time -= 1;
+      sekundyElm.textContent = String(time);
+    }
+    if (time <= 0) {
+      clearInterval(casovac);
+      alarmElm.classList.add('alarm--ring');
+      document.querySelector('audio').play();
+    }
+  };
+
+  const casovac = setInterval(odpocet, 1000);
+};
+
+const formularElm = document.querySelector('.controls');
+formularElm.addEventListener('submit', nastartujMinutku);
